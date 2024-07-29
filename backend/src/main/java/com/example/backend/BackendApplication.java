01dfee3897284249bc9d171da4f423552b173568
Jordan <jordan.gaillet16@gmail.com>
2024-07-29 22:21:59 +0200

    Correction classe main

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,21 +1,22 @@
 package com.example.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		//Charger les variables d'environnement à partir de .env
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		
-		//Lancer l'application Spring Boot
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Charger les variables d'environnement
+        String dbUrl = System.getenv("DB_URL");
+        String dbUsername = System.getenv("DB_USERNAME");
+        String dbPassword = System.getenv("DB_PASSWORD");
 
+        System.setProperty("DB_URL", dbUrl);
+        System.setProperty("DB_USERNAME", dbUsername);
+        System.setProperty("DB_PASSWORD", dbPassword);
+
+        // Lancer l'application Spring Boot
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
